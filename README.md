78aa612d62a030268b708edf7848a0b7b57ff342
Edward Tirado Jr <edward.tirado@outlook.com>
2014-11-10 21:51:35 -0700

    Added album check. Better naming for variables and functions.  Updated readme.

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ reddit-image-downloader
 =======================
 
 Allows users to get images from image subreddits quickly and easily
+
+Requires python 3 and PRAW ( https://github.com/praw-dev/praw ).
+
+Usage
+=====
+
+From the command line enter:
+1. python3 downloader.py NUMBER_OF_IMAGES DOWNLOAD_PATH SUBREDDIT_NAME
+2. ???
+3. Profit! (or just enjoy your brand new images)
+
+
+To-Do
+=====
+* Get images that are not linked directly or are in albums
+* Download the provided number of images even if some are skipped
+* Make compatible with python 2.6+
--- downloader.py
+++ downloader.py
@@ -1,14 +1,17 @@
-import sys, os, re
-import praw, urllib
+import sys, os, re, urllib
+import praw
 
 
-def dupeCheck(file_name, file_path):
+def isDuplicate(file_name, file_path):
 	file_list = os.listdir(file_path)
 	file_exists = file_name in file_list
 	return file_exists
 
-#def isAlbum(file_name):
-#	return re.match('\..+', file_name) == None
+def isAlbum(file_name):
+	split_name = file_name.split('.')
+	is_album = len(split_name) < 2
+	return is_album
+
 
 
 if len(sys.argv) < 4:
@@ -21,14 +24,13 @@ script, image_num, file_path, subreddit = sys.argv
 reddit_conn = praw.Reddit(user_agent='Reddit Image Downloader 1.0 by Ed Tirado')
 submissions = reddit_conn.get_subreddit(subreddit).get_hot(limit = int(image_num))
 
-for title in submissions:
-	link  = title.url
+for post in submissions:
+	link  = post.url
 	file_name = link.split('/')[-1]
 
-	if dupeCheck(file_name, file_path):
+	if isDuplicate(file_name, file_path) or isAlbum(file_name):
 		continue
-	
-	print('Downloading ' + file_name)
+
 	urllib.request.urlretrieve(link,file_path + '/' + file_name)
 
 
