0ec1cb7fe819df2c30508ea9c1ebf67cdfb0ca15
Edward Tirado Jr <edward.tirado@outlook.com>
2014-11-17 23:02:56 -0700

    PEP8 updates

--- downloader.py
+++ downloader.py
@@ -4,52 +4,55 @@ Author: Edward Tirado Jr
 License: MIT
 """
 
-import sys, os, re, urllib
+import sys
+import os
+import re
+import urllib
 import praw
 
-EXTENSIONS = set(['.jpg','.jpeg','.gif','.png'])
+EXTENSIONS = set(['.jpg', '.jpeg', '.gif', '.png'])
+
 
 def isDuplicate(file_name, file_path):
-	file_list = os.listdir(file_path)
-	file_exists = file_name in file_list
-	return file_exists
+    file_list = os.listdir(file_path)
+    file_exists = file_name in file_list
+    return file_exists
+
 
 def isAlbum(link, file_name):
-	is_imgur_album = re.match('https?://.*imgur.*\/a\/', link) is not None
-	is_imgur_gallery = re.match('https?://.*imgur.*gallery', link) is not None
-	has_file_extension = file_name.endswith(tuple(EXTENSIONS))
-	
-	if (not is_imgur_album and not is_imgur_gallery) or has_file_extension:
-		return False
-	else:
-		return True
+    is_imgur_album = re.match('https?://.*imgur.*\/a\/', link) is not None
+    is_imgur_gallery = re.match('https?://.*imgur.*gallery', link) is not None
+    has_file_extension = file_name.endswith(tuple(EXTENSIONS))
+
+    if (not is_imgur_album and not is_imgur_gallery) or has_file_extension:
+        return False
+    else:
+        return True
 
 
 if len(sys.argv) < 4:
-	print('Not enough arguments: \n' +
-		'Need number of images, download directory, and subreddit name')
-	sys.exit()
+    print('Not enough arguments: \n' +
+          'Need number of images, download directory, and subreddit name')
+    sys.exit()
 
 script, image_num, file_path, subreddit = sys.argv
 
 reddit_conn = praw.Reddit(user_agent='Reddit Image Downloader 1.0 by Ed Tirado')
-submissions = reddit_conn.get_subreddit(subreddit).get_hot(limit = int(image_num))
+submissions = reddit_conn.get_subreddit(subreddit).get_hot(limit=int(image_num))
 
 for post in submissions:
-	link  = post.url
-	file_name = link.split('/')[-1]
+    link = post.url
+    file_name = link.split('/')[-1]
 
-	if isDuplicate(file_name, file_path) or isAlbum(link, file_name):
-		continue
-	
-	#Allows images without extensions to download properly.  Need better solution - Ed
-	if not file_name.endswith(tuple(EXTENSIONS)):
-		link += '.jpg'
-		file_name += '.jpg'
+    if isDuplicate(file_name, file_path) or isAlbum(link, file_name):
+        continue
 
-	try:
-		urllib.request.urlretrieve(link,file_path + '/' + file_name)
-	except Exception:
-		urllib.urlretrieve(link,file_path + '/' + file_name) #python2 compatibility
+    # Allows images without extensions to download properly. Hacky...
+    if not file_name.endswith(tuple(EXTENSIONS)):
+        link += '.jpg'
+        file_name += '.jpg'
 
-
+    try:
+        urllib.request.urlretrieve(link, file_path + '/' + file_name)
+    except Exception:
+        urllib.urlretrieve(link, file_path + '/' + file_name)  # python2 compat
--- gnome3-bg-changer.py
+++ gnome3-bg-changer.py
@@ -6,21 +6,27 @@ License: MIT
 """
 
 from os import walk, path
-import sys, random, subprocess, time
+import sys
+import random
+import subprocess
+import time
+
 
 def getImages(image_path):
-	for(dirpath, dirnames, filenames) in walk(image_path):
-		images.extend(filenames)
-		break
+    for(dirpath, dirnames, filenames) in walk(image_path):
+        images.extend(filenames)
+        break
+
 
 def setRandomImage(image_path, images):
-	image_position = random.randrange(0, len(images))
-	current_image = images[image_position]
-	
-	subprocess.call('gsettings set org.gnome.desktop.background picture-uri file:///' +
-			path.abspath(image_path) + '/' + current_image, shell = True)
+    image_position = random.randrange(0, len(images))
+    current_image = images[image_position]
 
-	time.sleep(int(frequency))
+    subprocess.call(
+        'gsettings set org.gnome.desktop.background picture-uri file:///' +
+        path.abspath(image_path) + '/' + current_image, shell=True)
+
+    time.sleep(int(frequency))
 
 
 images = []
@@ -29,4 +35,4 @@ script, image_path, frequency = sys.argv
 getImages(image_path)
 
 while(True):
-	setRandomImage(image_path, images)
+    setRandomImage(image_path, images)
